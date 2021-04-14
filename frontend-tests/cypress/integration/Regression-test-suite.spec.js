aa79f72787d94d71878ce6923de2c1c494ce5aba
Birgersson <sandrabirger@gmail.com>
2021-04-14 21:15:49 +0200

    test done

--- frontend-tests/cypress/integration/Regression-test-suite.spec.js
+++ frontend-tests/cypress/integration/Regression-test-suite.spec.js
@@ -5,5 +5,13 @@ describe('Regression test suite',function(){
     //test case
     it('Perform valid login', function(){
         cy.visit('http://localhost:3000')
+        cy.contains('Login')
+        cy.title().should('include','Hotel')
+        cy.get(':nth-child(1) > input').type('tester01')  //Samma fält: div.field:nth-child(1) > input:nth-child(2) 
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.get('.user > .btn').click()
+        cy.contains('Login')
     })
 })
